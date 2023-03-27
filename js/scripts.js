104c83ae6bc9cbeca55814e438144bec71135448
jeffrey2801 <80184055+jeffrey2801@users.noreply.github.com>
2023-03-27 09:44:20 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,255 @@
+// SIDEBAR TOGGLE
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+
+function openSidebar() {
+  if(!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if(sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
+    sidebarOpen = false;
+  }
+}
+
+
+
+// ---------- CHARTS ----------
+
+// BAR CHART
+var barChartOptions = {
+  series: [{
+    data: [31, 40, 28, 51, 42, 19, 10, 18, 30, 20, 
+      43, 50, 68, 31, 52, 9, 10, 19, 35, 80, 
+      21, 46, 28, 58, 30, 19, 16, 13, 36, 13,],
+    name: "Count",
+  }],
+  chart: {
+    type: "bar",
+    background: "transparent",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: [
+    "#2962ff",
+    "#d50000",
+    "#2e7d32",
+    "#ff6d00",
+    "#583cb3",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: "40%",
+    }
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    opacity: 1,
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  stroke: {
+    colors: ["transparent"],
+    show: true,
+    width: 2
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  },
+  xaxis: {
+    categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
+    title: {
+      style: {
+        color: "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      show: true,
+      color: "#55596e",
+    },
+    axisTicks: {
+      show: true,
+      color: "#55596e",
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+      style: {
+        color:  "#f5f7ff",
+      },
+    },
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
+
+
+// AREA CHART
+var areaChartOptions = {
+  series: [{
+    name: "Tegangan",
+    data: [31, 40, 28, 51, 42, 109, 100, 190, 30, 20, 
+            43, 50, 68, 31, 52, 9, 10, 19, 35, 80, 
+            21, 46, 28, 58, 30, 19, 106, 13, 36, 186, ],
+  },],
+  chart: {
+    type: "area",
+    background: "transparent",
+    height: 350,
+    stacked: false,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#d50000"],
+  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+            '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+            '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
+  dataLabels: {
+    enabled: false,
+  },
+  fill: {
+    gradient: {
+      opacityFrom: 0.4,
+      opacityTo: 0.1,
+      shadeIntensity: 1,
+      stops: [0, 100],
+      type: "vertical",
+    },
+    type: "gradient",
+  },
+  grid: {
+    borderColor: "#55596e",
+    yaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+  },
+  legend: {
+    labels: {
+      colors: "#f5f7ff",
+    },
+    show: true,
+    position: "top",
+  },
+  markers: {
+    size: 6,
+    strokeColors: "#1b2635",
+    strokeWidth: 3,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    axisBorder: {
+      color: "#55596e",
+      show: true,
+    },
+    axisTicks: {
+      color: "#55596e",
+      show: true,
+    },
+    labels: {
+      offsetY: 5,
+      style: {
+        colors: "#f5f7ff",
+      },
+    },
+  },
+  yaxis: 
+  [
+    {
+      title: {
+        style: {
+          color: "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        style: {
+          color:  "#f5f7ff",
+        },
+      },
+      labels: {
+        style: {
+          colors: ["#f5f7ff"],
+        },
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+    theme: "dark",
+  }
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
